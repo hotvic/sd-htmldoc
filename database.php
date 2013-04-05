69f323722aa7da5089681cb273f74a33cf8dbf63
Victor Aurélio <aurelio@archlinux.info>
2013-04-04 21:42:01 -0300

    now using mysqli

--- database.php
+++ database.php
@@ -1,72 +1,53 @@
 <?php
-class UserDB {
-    private $file = FALSE;
-    private $open = FALSE;
 
-    function opendb($dbfilename){
-        $this->file = fopen($dbfilename, "c+");
-        if ( $this->file == FALSE ) die("Couldn't Open Database file, Sorry!");
-        $this->open = TRUE;
+// Configuration
+$db_user = 'user';
+$db_pass = 'pass';
+$db_host = 'localhost';
+$db_base = 'sd-hmldoc';
+// End Configuration
+
+// Global function(s)
+function sec_session_start() {
+    $session_name = 'sd-htmldoc_login';
+    $secure       = false;
+    $httponly     = true;
+
+    ini_set('session.use_only_cookies', 1);
+    $cookieParams = session_get_cookie_params();
+    session_set_cookie_params($cookieParams["lifetime"], $cookieParams["path"], $cookieParams["domain"], $secure, $httponly); 
+    session_name($session_name);
+    session_start();
+    session_regenerate_id(true);
+}
+
+// Main database class
+class Database {
+    private $mysqli;
+
+    function __construct() {
+        $this->mysqli = new mysqli($db_host, $db_user, $db_base);
+
+        if (! $this->mysqli ) return false;
     }
-    function closedb(){
-        if ( $this->open ) fclose($this->file);
-        else die("Database is currently not open, cannot close then!");
-    }
-    /*
-     * +--------------+--------------+
-     * | 0 = ID       | 1 = nick     |
-     * +--------------+--------------+
-     * | 2 = password | 3 = realname |
-     * +-----------------------------+
-     * | 4 = number of posts         |
-     * +-----------------------------+
-     */
-    function queryUser($where, $who = 0){
-        $user = FALSE;
 
-        while ( ! feof($this->file) ){
-            $aignore = array("^#", "^$", "^ *$");
-            $line = fgets($this->file);
+    function login_check($user, $pass = false) {
+        if ( $stmt = $this->mysqli->prepare("SELECT id, password, salt FROM authors WHERE username = ? LIMIT 1") ){
+            $stmt->bind_param('s', $user);
+            $stmt->execute();
+            $stmt->bind_result($id, $password, $salt);
+            $stmt->fetch();
 
-            foreach ($aignore as $value){
-                if ( preg_match("@$value@", $line) )
-                    continue 2;
+            if ( $smtp->num_rows == 1 ){
+
+            } else {
+                return false;
             }
+            $stmt->close();
 
-            $u = explode(":", $line);
-
-            if ( $u[$who] == $where ){
-                $user = $u;
-                rewind($this->file);
-                break;
-            }
+        } else {
+            return false;
         }
-        return $user;
-    }
-    function addUser($user){
-        $id = isset($user["id"]) ? $user["id"] : $this->getnewID();
-        $fa = array($id, $user["nick"], $user["pass"],
-                    $user["real"], $user["num"]);
-        $u = implode(":", $fa);
-
-        fseek($this->file, 0, SEEK_END);
-        fwrite($this->file, $u . PHP_EOL);
-        rewind($this->file);
-    }
-    function getnewID(){
-        $last = 0;
-
-        while ( ! feof($this->file) ){
-            $line = fgets($this->file);
-
-            if ( $line[0] == "#" || $line[0] == "\n" ) continue;
-
-            $cur = (int) explode(":", $line)[0];
-            $last = $cur < $last ? $last : $cur;
-        }
-        rewind($this->file);
-
-        return $last < 1001 ? 1001 : $last + 1;
     }
 }
 ?>
